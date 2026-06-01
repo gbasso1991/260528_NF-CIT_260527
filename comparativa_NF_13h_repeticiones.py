9a9e1d807d756fb803361b35c8946b8aa7c7f62e
Giuliano <giulianoandresbasso@gmail.com>
2026-06-01 10:08:55 -0300

    Concentracion correcta

--- comparativa_NF_13h_repeticiones.py
+++ comparativa_NF_13h_repeticiones.py
@@ -128,12 +128,12 @@ def extraer_SAR_tau(resultados):
     return SAR, tau, Hc
 #%% Obtengo ciclos y resultados para cada concentracion - Todo a 300 kHz
 
-ciclos_primera = glob("**/**/*ciclo_promedio_H_M.txt",recursive=True)
-resultados_primera = glob("**/**/*resultados.txt",recursive=True)
+ciclos_primera = glob("data/**/*ciclo_promedio_H_M.txt",recursive=True)
+resultados_primera = glob("data/**/*resultados.txt",recursive=True)
 
 ciclos_primera.sort()
 resultados_primera.sort()
-conc_primera = 33.33 #g/L
+conc_primera =  19.8 #g/L
 
 for p in ciclos_primera:
     print('  ',p)
@@ -141,7 +141,7 @@ for p in ciclos_primera:
 for res in resultados_primera:
     print('  ',res)
 print('-'*50)    
-#% ploteo 
+#%% ploteo ciclos 
 fig00, ax =plt.subplots(figsize=(8,6),constrained_layout=True,sharey=True,sharex=True)
 
 ax.set_ylabel('M (A/m)')
@@ -154,8 +154,10 @@ for i,e in enumerate(ciclos_primera):
 ax.grid()
 ax.set_xlabel('H (kA/m)')
 ax.legend(loc='upper left',frameon=True,shadow=True)
-plt.suptitle(f'Comparativa ciclos promedio NF@cit primera hs\n300 kHz & 58 kA/m')
-# plt.savefig('0_ciclos_promedio_NF13h_AV_AN.png',dpi=300)
+plt.suptitle(f'Comparativa ciclos promedio NF@cit 260527\n300 kHz & 58 kA/m')
+plt.savefig('0_ciclos_promedio_NF@cit_260527.png',dpi=300)
+
+#%%
 SAR_primera, tau_primera, Hc_primera = extraer_SAR_tau(resultados_primera)
 res_primera=[]
 print('Resultados primera', '='*80,'\n')
@@ -169,271 +171,24 @@ for i,r in enumerate(res_primera):
     rate=dT/dt
     print(f'WRate = {rate:.2f} °C/s')
     rates_primera.append(rate)
-
+print('-'*50)
 print(f"ESAR primera: {np.mean(SAR_primera)}")
 print(f" tau primera: {np.mean(tau_primera)}") 
 print(f"  Hc primera: {np.mean(Hc_primera)}")
 print(f"  WRate: {ufloat(np.mean(rates_primera),np.std(rates_primera)):.1uS} °C/s")
-
-#%% buena
-ciclos_buena = glob("2_buena_260409/**/*ciclo_promedio_H_M.txt",recursive=True)
-resultados_buena = glob("2_buena_260409/**/*resultados.txt",recursive=True)
-
-ciclos_buena.sort()
-resultados_buena.sort()
-conc_buena = 32.8 #g/L
-
-for p in ciclos_buena:
-    print('  ',p)
-
-for res in resultados_buena:
-    print('  ',res)
-print('-'*50)    
-#% ploteo 
-fig01, ax =plt.subplots(figsize=(8,6),constrained_layout=True,sharey=True,sharex=True)
-
-ax.set_ylabel('M (A/m)')
-
-for i,e in enumerate(ciclos_buena):
+print('-'*50)
+#%% Ploteo comparativa templogs
+fig, ax = plt.subplots(figsize=(8,6),constrained_layout=True)
+for i,e in enumerate(ciclos_primera):
     if '152dA' in e:
-        _,_,_, H_buena,M_buena,_ = lector_ciclos(ciclos_buena[i])
-        ax.plot(H_buena/1000,M_buena,'-',label=f'NF{i}')
-
+        _,_,time,temperatura,_,_,_,_,_,_,_,_,_,_,_ = lector_resultados(resultados_primera[i])
+        ax.plot(time-time[0], temperatura, '.-', label=f'NF{i}')
 ax.grid()
-ax.set_xlabel('H (kA/m)')
+ax.set_ylabel('T (°C)')
+ax.set_xlabel('Time (s)')
 ax.legend(loc='upper left',frameon=True,shadow=True)
-plt.suptitle(f'Comparativa ciclos promedio NF@cit buena hs\n300 kHz & 58 kA/m')
-# plt.savefig('0_ciclos_promedio_NF13h_AV_AN.png',dpi=300)
-SAR_buena, tau_buena, Hc_buena = extraer_SAR_tau(resultados_buena)
-res_buena=[]
-print('Resultados buena', '='*80,'\n')
-for r in resultados_buena:
-    res_buena.append(ResultadosESAR(os.path.dirname(r)))
-
-rates_buena = []
-for i,r in enumerate(res_buena):
-    dt = r.time[-1]-r.time[0]
-    dT = r.temperatura[-1]-r.temperatura[0]
-    rate=dT/dt
-    print(f'WRate = {rate:.2f} °C/s')
-    rates_buena.append(rate)
-print(f"ESAR buena: {np.mean(SAR_buena)}")
-print(f" tau buena: {np.mean(tau_buena)}") 
-print(f"  Hc buena: {np.mean(Hc_buena)}")
-print(f"rate buena: {ufloat(np.mean(rates_buena),np.std(rates_buena)):.1uS} °C/s")
-#%% Autoclave Viejo
-ciclos_AV = glob("3_AV_260421/**/*ciclo_promedio_H_M.txt",recursive=True)
-resultados_AV = glob("3_AV_260421/**/*resultados.txt",recursive=True)
-
-ciclos_AV.sort()
-resultados_AV.sort()
-conc_AV = 8.46 #g/L
-
-for p in ciclos_AV:
-    print('  ',p)
-
-for res in resultados_AV:
-    print('  ',res)
-print('-'*50)    
-#% ploteo 
-fig01, ax =plt.subplots(figsize=(8,6),constrained_layout=True,sharey=True,sharex=True)
-
-ax.set_ylabel('M (A/m)')
-
-for i,e in enumerate(ciclos_AV):
-    if '152dA' in e:
-        _,_,_, H_AV,M_AV,_ = lector_ciclos(ciclos_AV[i])
-        ax.plot(H_AV/1000,M_AV,'-',label=f'NF{i}')
-
-ax.grid()
-ax.set_xlabel('H (kA/m)')
-ax.legend(loc='upper left',frameon=True,shadow=True)
-plt.suptitle(f'Comparativa ciclos promedio NF@cit AV hs\n300 kHz & 58 kA/m')
-# plt.savefig('0_ciclos_promedio_NF13h_AV_AN.png',dpi=300)
-SAR_AV, tau_AV, Hc_AV = extraer_SAR_tau(resultados_AV)
-
-res_AV=[]
-print('Resultados AV', '='*80,'\n')
-for r in resultados_AV:
-    res_AV.append(ResultadosESAR(os.path.dirname(r)))
-
-rates_AV = []
-for i,r in enumerate(res_AV):
-    dt = r.time[-1]-r.time[0]
-    dT = r.temperatura[-1]-r.temperatura[0]
-    rate=dT/dt
-    print(f'WRate = {rate:.2f} °C/s')
-    rates_AV.append(rate)
-
-print(f"ESAR AV: {np.mean(SAR_AV)}")
-
-print(f" tau AV: {np.mean(tau_AV)}") 
-print(f"  Hc AV: {np.mean(Hc_AV)}")
-print(f"rate AV: {ufloat(np.mean(rates_AV),np.std(rates_AV)):.1uS} °C/s")
-
-#%% Importo ciclos y resultados para buena sintesis 
-# NF@cit 13 hs - 260409
-sintesis_buena = '260409 buena'
-#pc portatil
-#ciclos_13_buena = glob("../260410_ESAR_c_Temp_NF-cit_13h_260409/152dA/**/*ciclo_promedio_H_M.txt",recursive=True)
-#resultados_13_buena = glob("../260410_ESAR_c_Temp_NF-cit_13h_260409/152dA/**/*resultados.txt",recursive=True)   
-
-#pc escritorio
-ciclos_13_buena = glob("../260410_NF@cit_13h_260409_ESAR_c_Temp/152dA/**/*ciclo_promedio_H_M.txt",recursive=True)
-resultados_13_buena = glob("../260410_NF@cit_13h_260409_ESAR_c_Temp/152dA/**/*resultados.txt",recursive=True)
-ciclos_13_buena.sort()
-resultados_13_buena.sort()
-conc_13_buena = 32.8 #g/L
-
-for p in ciclos_13_buena:
-    print('  ',p)
-
-for res in resultados_13_buena:
-    print('  ',res)
-print('-'*50)   
-# ploteo
-
-fig02, ax =plt.subplots(1,1,figsize=(7,6),constrained_layout=True)  
-ax.set_ylabel('M (A/m)')
-ax.set_title(f'NF@cit 13 hs - {conc_13_buena:.1f} g/L',loc='left')      
-for i,e in enumerate(ciclos_13_buena):
-    if '152dA' in e:
-        _,_,_, H_13,M_13,_ = lector_ciclos(ciclos_13_buena[i])
-        ax.plot(H_13/1000,M_13,'-',label=f'NF{i}')
-ax.grid()
-ax.set_xlabel('H (kA/m)')
-ax.legend(loc='upper left')
-#plt.suptitle(f'Ciclos promedio NF@cit 13 hs {sintesis_buena}\n300 kHz & 58 kA/m')          
-# plt.savefig('presentacion_:0_ciclos_promedio_NF13h_buena.png',dpi=300)
-#%% Importo ciclos y resultados para sintesis mala
-# NF@cit 13 hs - 260409
-sintesis_mala = '260330 mala'
-# pc portatil
-# ciclos_13_mala = glob("../26040
-# 6_ESAR_c_Temp_NF-cit_13h_260330/57_150/**/*ciclo_promedio_H_M.txt",recursive=True)
-# resultados_13_mala = glob("../260406_ESAR_c_Temp_NF-cit_13h_260330/57_150/**/*resultados.txt",recursive=True)
-
-# pc escritorio
-ciclos_13_mala = glob("../260406_NF@cit_13h_260330_ESAR_c_Temp/57_150/**/*ciclo_promedio_H_M.txt",recursive=True)
-resultados_13_mala = glob("../260406_NF@cit_13h_260330_ESAR_c_Temp/57_150/**/*resultados.txt",recursive=True)
-
-ciclos_13_mala.sort()
-resultados_13_mala.sort()
-conc_13_mala = 24.0 #g/L
-ciclos_13_mala.pop(1) #saco outliers
-resultados_13_mala.pop(1) 
-for p in ciclos_13_mala:
-    print('  ',p)
-
-for res in resultados_13_mala:
-    print('  ',res)
-print('-'*50)   
-# ploteo
-
-fig02, ax =plt.subplots(1,1,figsize=(6,5),constrained_layout=True)  
-ax.set_ylabel('M (A/m)')
-ax.set_title(f'NF@cit 13 hs - {conc_13_mala:.1f} g/L',loc='left')      
-for i,e in enumerate(ciclos_13_mala):
-    if '150dA' in e:
-        _,_,_, H_13,M_13,_ = lector_ciclos(ciclos_13_mala[i])
-        ax.plot(H_13/1000,M_13,'-',label=f'NF{i}')
-ax.grid()
-ax.set_xlabel('H (kA/m)')
-ax.legend(loc='upper left')
-plt.suptitle(f'Ciclos promedio NF@cit 13 hs {sintesis_mala}\n300 kHz & 58 kA/m')          
-
-#%% COMPARO LAS 4 SINTESIS en grafico 2x2 
-#%% primero extraigo SAR y tau de resultados para cada sintesis
-def extraer_SAR_tau(resultados):
-    SAR = []
-    tau = []
-    Hc = []
-    for res in resultados:
-        meta,_,_,_,_,_,_,_,_,_,_,_,_,_,_ = lector_resultados(res)   
-        SAR.append(meta['SAR_W/g'])
-        tau.append(meta['tau_ns'])
-        Hc.append(meta['Hc_kA/m']) 
-    return SAR, tau, Hc
-SAR_13_AV, tau_13_AV, Hc_13_AV = extraer_SAR_tau(resultados_13_AV)
-SAR_13_AN, tau_13_AN, Hc_13_AN = extraer_SAR_tau(resultados_13_AN)
-SAR_13_buena, tau_13_buena, Hc_13_buena = extraer_SAR_tau(resultados_13_buena)
-SAR_13_mala, tau_13_mala, Hc_13_mala = extraer_SAR_tau(resultados_13_mala)    
-#%% Ploteo comparativo de las 4 sintesis en grafico 2x2, normalizando por concentracion y con SAR en la leyenda
-
-fig5 , axs = plt.subplots(2, 2, figsize=(12, 10), constrained_layout=True, sharex=True, sharey=True)
-axs[0, 0].set_title(f'{sintesis_AV} - {conc_13_AV:.1f} g/L', loc='left')
-axs[0, 1].set_title(f'{sintesis_AN} - {conc_13_AN:.1f} g/L', loc='left')
-axs[1, 0].set_title(f'{sintesis_buena} - {conc_13_buena:.1f} g/L', loc='left')
-axs[1, 1].set_title(f'{sintesis_mala} - {conc_13_mala:.1f} g/L', loc='left')
-
-for i,e in enumerate(ciclos_13_AV):
-    if '152dA' in e:
-        _,_,_, H_13,M_13,_ = lector_ciclos(ciclos_13_AV[i])
-        axs[0, 0].plot(H_13/1000,M_13/conc_13_AV,'-',label=f'{SAR_13_AV[i]:.3uS}')
-
-for i,e in enumerate(ciclos_13_AN):
-    if '152dA' in e:
-        _,_,_, H_13,M_13,_ = lector_ciclos(ciclos_13_AN[i])
-        axs[0, 1].plot(H_13/1000,M_13/conc_13_AN,'-',label=f'{SAR_13_AN[i]:.3uS}')
-
-for i,e in enumerate(ciclos_13_buena):
-    if '152dA' in e:
-        _,_,_, H_13,M_13,_ = lector_ciclos(ciclos_13_buena[i])
-        axs[1, 0].plot(H_13/1000,M_13/conc_13_buena,'-',label=f'{SAR_13_buena[i]:.3uS}')
-
-for i,e in enumerate(ciclos_13_mala):
-    if '150dA' in e:
-        _,_,_, H_13,M_13,_ = lector_ciclos(ciclos_13_mala[i])
-        axs[1, 1].plot(H_13/1000,M_13/conc_13_mala,'-',label=f'{SAR_13_mala[i]:.3uS}')
-
-for a in axs.flatten():
-    a.grid()
-    a.legend(title='SAR (W/g)',loc='upper left',frameon=True,shadow=True)
-
-for a in [axs[1, 0], axs[1, 1]]:
-    a.set_xlabel('H (kA/m)') 
-for a in [axs[0, 0], axs[1, 0]]:
-    a.set_ylabel('M/[NPM] (Am²/kg)') 
-plt.suptitle(f'Comparativa sintesis NF@cit 13 hs\n300 kHz & 58 kA/m')
-#%% Agrego 1er sintesis de 13 hs
-# NF@cit 13 hs - 260409
-sintesis_primera = '260306 primera'
-#pc portatil
-#ciclos_13_primera = glob("../260410_ESAR_c_Temp_NF-cit_13h_260409/152dA/**/*ciclo_promedio_H_M.txt",recursive=True)
-#resultados_13_primera = glob("../260410_ESAR_c_Temp_NF-cit_13h_260409/152dA/**/*resultados.txt",recursive=True)   
-
-#pc escritorio
-ciclos_13_primera = glob("../260306_NF@cit_8_13_18_hs_260306_ESAR_c_Temp/NF13hs/150dA/**/*ciclo_promedio_H_M.txt",recursive=True)
-resultados_13_primera = glob("../260306_NF@cit_8_13_18_hs_260306_ESAR_c_Temp/NF13hs/150dA/**/*resultados.txt",recursive=True)
-ciclos_13_primera.sort()
-resultados_13_primera.sort()
-conc_13_primera = 26.7 #g/L
-
-for p in ciclos_13_primera:
-    print('  ',p)
-
-for res in resultados_13_primera:
-    print('  ',res)
-print('-'*50)   
-# ploteo
-
-fig02, ax =plt.subplots(1,1,figsize=(6,5),constrained_layout=True)  
-ax.set_ylabel('M (A/m)')
-ax.set_title(f'NF@cit 13 hs - {conc_13_primera:.1f} g/L',loc='left')      
-for i,e in enumerate(ciclos_13_primera):
-    if '150dA' in e:
-        _,_,_, H_13,M_13,_ = lector_ciclos(ciclos_13_primera[i])
-        ax.plot(H_13/1000,M_13,'-',label=f'NF{i}')
-ax.grid()
-ax.set_xlabel('H (kA/m)')
-ax.legend(loc='upper left')
-plt.suptitle(f'Ciclos promedio NF@cit 13 hs {sintesis_primera}\n300 kHz & 58 kA/m')          
-
-#extraigo ESAR, tau y Hc de la primera sintesis
-#saco ultimo dato de res por conveniencia estetica en los graficos. Los resultados son casi iguales
-resultados_13_primera.pop(-1)
-ciclos_13_primera.pop(-1)
-SAR_13_primera, tau_13_primera, Hc_13_primera = extraer_SAR_tau(resultados_13_primera)  
+plt.suptitle(f'Templogs NF@cit 260527\n300 kHz & 58 kA/m')
+plt.savefig('0_templogs_NF@cit_260527.png',dpi=300)
 # %% ploteo comparativo de errorbars de ESAR
 cuadro = '$f=300$ kHz\n$H_0=58$ kA/m'
 categorias = ['260306\nprimera','260409\nmala','260409\nbuena','260421\nAutoclave Viejo', '260421\nAutoclave Nuevo']
@@ -446,17 +201,18 @@ sep = 0.25
 for i,s in enumerate(SAR_13_primera):
     ax.bar(i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C0')
 
-for i,s in enumerate(SAR_13_mala):
-    ax.bar(1+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C3')
+# for i,s in enumerate(SAR_13_mala):
 
-for i,s in enumerate(SAR_13_buena):
-    ax.bar(2+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C2')
+#     ax.bar(1+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C3')
 
-for i,s in enumerate(SAR_13_AV):
-    ax.bar(3+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C1')
+# for i,s in enumerate(SAR_13_buena):
+#     ax.bar(2+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C2')
 
-for i,s in enumerate(SAR_13_AN):
-    ax.bar(4+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C4')
+# for i,s in enumerate(SAR_13_AV):
+#     ax.bar(3+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C1')
+
+# for i,s in enumerate(SAR_13_AN):
+#     ax.bar(4+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C4')
 
 
 ax.set_xticks(x)
@@ -477,17 +233,17 @@ sep = 0.25
 for i,s in enumerate(tau_13_primera):
     ax.bar(i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C0')
 
-for i,s in enumerate(tau_13_mala):
-    ax.bar(1+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C3')
+# for i,s in enumerate(tau_13_mala):
+#     ax.bar(1+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C3')
 
-for i,s in enumerate(tau_13_buena):
-    ax.bar(2+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C2')
+# for i,s in enumerate(tau_13_buena):
+#     ax.bar(2+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C2')
 
-for i,s in enumerate(tau_13_AV):
-    ax.bar(3+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C1')
+# for i,s in enumerate(tau_13_AV):
+#     ax.bar(3+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C1')
 
-for i,s in enumerate(tau_13_AN):
-    ax.bar(4+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C4')
+# for i,s in enumerate(tau_13_AN):
+#     ax.bar(4+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C4')
 
 ax.set_xticks(x)
 ax.set_xticklabels(categorias)
@@ -507,17 +263,18 @@ sep = 0.25
 for i,s in enumerate(Hc_13_primera):
     ax.bar(i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C0')
 
-for i,s in enumerate(Hc_13_mala):
-    ax.bar(1+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C3')
+# for i,s in enumerate(Hc_13_mala):
+#     ax.bar(1+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C3')
 
-for i,s in enumerate(Hc_13_buena):
-    ax.bar(2+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C2')
+# for i,s in enumerate(Hc_13_buena):
+#     ax.bar(2+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C2')
 
-for i,s in enumerate(Hc_13_AV):
-    ax.bar(3+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C1')
+# for i,s in enumerate(Hc_13_AV):
+#     ax.bar(3+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C1')
 
-for i,s in enumerate(Hc_13_AN):
-    ax.bar(4+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C4')
+# for i,s in enumerate(Hc_13_AN):
+#     ax.bar(4+i*sep-sep, s.n, yerr=s.s, width=0.2, capsize=5, color='C4')
+
 
 
 ax.set_xticks(x)
